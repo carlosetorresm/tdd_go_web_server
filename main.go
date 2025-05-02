70333561bfe75d784f7a1c7a990dc7cd929d22b3
Carlos Eugenio Torres Mota <teugenio@tresguerras.com.mx>
2025-05-02 09:58:32 -0600

    implementation of the file store in the main server, removing the in_memory storage

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
-	interactions "github.com/carlosetorresm/tdd_go_web_server/domain/interactions"
+	filesystem "github.com/carlosetorresm/tdd_go_web_server/domain/file_system"
 	"github.com/carlosetorresm/tdd_go_web_server/server"
 )
 
+const dbFileName = "game.db.json"
+const port = 5000
+
 func main() {
-	store := interactions.NewInMemoryPlayerStore()
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatalf("problem opening %s %v", dbFileName, err)
+	}
+
+	store := &filesystem.FileSystemPlayerStore{Database: db}
 	server := server.NewPlayerServer(store)
-	log.Fatal(http.ListenAndServe(":5000", server))
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), server); err != nil {
+		log.Fatalf("could not liten on port %d %v", port, err)
+	}
 }
